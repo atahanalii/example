458969476661f230e80e920bf8b02a9c865cfd1b
atahanalii <atahanalii@gmail.com>
2014-10-08 13:57:54 +0200

    atahan

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 0;
+            string a = "**";
+            while (i<5)
+
+            {
+                i++;
+
+                while (j < i)
+                {
+                   Console.Write(a);
+                    a = a + "**";
+                    j++;
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+        }
+    }
+}
